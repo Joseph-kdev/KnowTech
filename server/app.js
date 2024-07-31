02e53a3da98071a65e62801b035f727497cad24c
Joseph-kdev <cjkinuthia5@gmail.com>
2024-07-31 18:51:40 +0300

    cors updated

--- server/app.js
+++ server/app.js
@@ -9,7 +9,14 @@ const app = express();
 const cors = require("cors")
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
+}));
+
 
 
 app.use(express.json())
