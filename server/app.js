853ae8b57ec8c291763b82cc364cfa6c5db25881
Joseph-kdev <cjkinuthia5@gmail.com>
2024-07-31 22:21:20 +0300

    resolve conflicts

--- server/app.js
+++ server/app.js
@@ -8,19 +8,8 @@ const express = require("express")
 const app = express();
 const cors = require("cors")
 
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200
-}));
-
-
-
+app.use(cors());
 app.use(express.json())
-
 app.use('/api/news', newsRoutes)
 app.use('/api/articles', articleRoutes)
 app.use('/api/chat', newsChatRoutes)
