6261adfc38aef2178ac2b8d0041f92bc46759ff3
Joseph-kdev <cjkinuthia5@gmail.com>
2024-07-31 16:39:06 +0300

    slight change

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const service = require("../.firebase/service-account.json")
+const service = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
     credential: admin.credential.cert(service)
